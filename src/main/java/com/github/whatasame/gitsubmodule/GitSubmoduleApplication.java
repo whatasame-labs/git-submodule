55285c48a107ab0e717ea0a6d7dacaa6b7c53b22
whatasame <97666463+whatasame@users.noreply.github.com>
2023-12-15 20:47:18 +0900

    Setup project

---
+++ src/main/java/com/github/whatasame/gitsubmodule/GitSubmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.github.whatasame.gitsubmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSubmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSubmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/whatasame/gitsubmodule/GitSubmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.whatasame.gitsubmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSubmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
